70662a52152171e5fc2b8a51c8b6d592c20c3520
JaiS739 <jksingh00739@gmail.com>
2022-06-04 13:02:21 +0530

    first commit

--- src/component/Timer.jsx
+++ src/component/Timer.jsx
@@ -1,24 +1,78 @@
 import React from "react";
+import { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 
 const Timer = () => {
-  const [timer, setTimer] = useState(10);
+  const [datas, setDatas] = useState([]);
+
+  const [saveValue, setSaveValue] = useState({
+    startvalue: "",
+    endsvalue: "",
+  });
+
+  console.log("hi", typeof datas, datas);
+
+  const [endDatas, setEndDatas] = useState([]);
+  console.log("hi", typeof endDatas, endDatas);
 
   useEffect(() => {
     const id = setInterval(() => {
-      if (timer < 1) {
+      if (datas >= endDatas) {
         clearInterval(id);
       } else {
-        setTimer(timer - 1);
+        setDatas(datas + 1);
+        // setTimer(timer + 1);
       }
       //   prev it is previous value of timer
     }, 1000);
     return () => {
       clearInterval(id);
     };
-  }, [timer]);
-  return <div>Timer:{timer}</div>;
+  }, [datas]);
+
+  let handleChange = (e) => {
+    let { name, value } = e.target;
+
+    setSaveValue({ ...saveValue, [name]: value });
+  };
+
+  const startRef = useRef();
+  const endRef = useRef();
+
+  let handleSubmit = () => {
+    setDatas(Number(saveValue.startvalue));
+    setEndDatas(Number(saveValue.endsvalue));
+    startRef.current.value = "";
+    endRef.current.value = "";
+  };
+
+  // ==================handleChang===============:
+
+  return (
+    <div>
+      Timer:{datas}
+      <div>
+        <input
+          type="number"
+          placeholder="starting second"
+          onChange={handleChange}
+          name="startvalue"
+          ref={startRef}
+        />
+        <input
+          type="number"
+          placeholder="ending second"
+          onChange={handleChange}
+          name="endsvalue"
+          ref={endRef}
+        />
+      </div>
+      <div>
+        <button onClick={handleSubmit}>submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default Timer;
